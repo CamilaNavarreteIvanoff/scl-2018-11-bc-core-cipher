db102df7c5eb3d46a023705fb98b59b3d7454e65
CamilaNavarreteIvanoff <cami.ilanavarrete@gmail.com>
2018-12-03 12:47:53 -0300

    mi primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,50 @@
 window.cipher = {
-  encode: () => {
+
+  encode: (offset,texto) => {let resultado = '';
+  offset= parseInt(offset);
+
+  for (let i=0; i<texto.length; i++){
+    let letraAscii=texto[i].charCodeAt();
+    if (letraAscii >= 65 && letraAscii <= 90 ) {
+      let letraConvertida=(letraAscii-65+offset)%26+65; 
+      resultado += String.fromCharCode(letraConvertida);
+
+         }else if (letraAscii >= 97 && letraAscii <= 122 ) {
+      let letraConvertida=(letraAscii-97+offset)%26+97; 
+      resultado += String.fromCharCode(letraConvertida);
+    }else{
+      resultado += texto[i]
+    }
+    
+    }
+    return resultado;
     /* Acá va tu código */
   },
-  decode: () => {
+
+  decode: (offset,texto) => {
+    let resultado = '';
+    offset= parseInt(offset);
+
+  for (let i=0; i<texto.length; i++){
+    let letraAscii=texto[i].charCodeAt();
+    if (letraAscii >= 65 && letraAscii <= 90 ) {
+      let letraConvertida=(letraAscii-65-offset)%26+65;
+      if (letraConvertida<65){
+        letraConvertida+=26;
+      }
+      resultado += String.fromCharCode(letraConvertida);
+    }else if (letraAscii >= 97 && letraAscii <= 122 ) {
+      let letraConvertida=(letraAscii-97-offset)%26+97;
+      if (letraConvertida<97){
+        letraConvertida+=26;
+      }
+      resultado += String.fromCharCode(letraConvertida);
+    }else{
+      resultado += texto[i]
+    }
+    
+    }
+    return resultado;
     /* Acá va tu código */
   }
 };
