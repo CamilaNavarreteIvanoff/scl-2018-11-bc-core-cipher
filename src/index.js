50001670c7df4cfc7e6603ef1308970c6d8b39f9
CamilaNavarreteIvanoff <cami.ilanavarrete@gmail.com>
2018-12-04 11:50:58 -0300

    avances css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
-function myfunction(){
-    document.getElementById("uno").innerHTML = `
-         <div >
-         <textarea type="text" id="mensaje" placeholder="escribe tu mensaje"></textarea>
-       </div>
-   
-       <div>
-         <input type="text" placeholder="offset" id=offset>
-       </div>
-   
-       <button onclick="cifrar()">cifrar</button>
-       <button onclick="descifrar()">descifrar</button>
-   
-       <div >
-           <textarea type="text" id="resultado" rows="10" cols="50" placeholder="resultado"></textarea>
-         </div>
-        `; }
+document.getElementById("botonUno").addEventListener("click", (evento) =>{
+    evento.preventDefault();
+
+    document.getElementById("divUno").style.display="none";
+    document.getElementById("divDos").style.display="block";
+
+})
+         
      
      
          
@@ -22,9 +13,10 @@ function myfunction(){
 
 
 function cifrar(){
-    //console.log("funciona");
+    console.log("funciona");
     let texto= document.getElementById("mensaje").value;
     let offset= document.getElementById("offset").value;
+    console.log(offset);
     
     let result= window.cipher.encode(offset, texto);
     //console.log(result);
