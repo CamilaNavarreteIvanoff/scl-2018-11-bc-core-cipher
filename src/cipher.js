cdf7c3c3617c68294d1b1a79f10c496ceda8db79
CamilaNavarreteIvanoff <cami.ilanavarrete@gmail.com>
2018-12-11 09:48:46 -0300

    cipher: offset negativo

--- src/cipher.js
+++ src/cipher.js
@@ -2,22 +2,31 @@ window.cipher = {
 
   encode: (offset,texto) => {
     let resultado = '';
-    offset= parseInt(offset);
+    offset= parseInt(offset);    // para traer el valor como numero y no string
+    if (offset <0){
+     offset= offset *-1 ;}
 
-  for (let i=0; i<texto.length; i++){
-    let letraAscii=texto[i].charCodeAt();
-    if (letraAscii >= 65 && letraAscii <= 90 ) {
-      let letraConvertida=(letraAscii-65+ offset)%26+65;
-      //console.log(letraAscii);
-      //console.log(offset);
-      resultado += String.fromCharCode(letraConvertida);
+  for (let i=0; i<texto.length; i++){ //para recorrer el string-lenght para saber su longitud
+    let letraAscii=texto[i].charCodeAt(); //devuelve la posición de cada letra
+        
+     //if (letraAscii >= 160 && letraAscii <= 255 ) {
+     // let letraConvertida=(letraAscii-160+offset)%26+160; 
+     // resultado += String.fromCharCode(letraConvertida);}
+
+    if (letraAscii >= 65 && letraAscii <= 90 ) { //formula para obtener el codigo ascii
+        let letraConvertida=(letraAscii-65+ offset )%26+65;
+        resultado += String.fromCharCode(letraConvertida);}//me devuelve la cadena completa de texto
      
-
-         }else if (letraAscii >= 97 && letraAscii <= 122 ) {
-      let letraConvertida=(letraAscii-97+offset)%26+97; 
-      resultado += String.fromCharCode(letraConvertida);
-    }else{
-      resultado += texto[i]
+    else if (letraAscii >= 97 && letraAscii <= 122 ) {
+        let letraConvertida=(letraAscii-97+offset)%26+97; 
+        resultado += String.fromCharCode(letraConvertida);}
+      
+      //else if (letraAscii === 164 ) {
+      //  let letraConvertida=(letraAscii+offset); 
+      //  resultado += String.fromCharCode(letraConvertida);}
+    
+     else{
+      resultado += texto[i]//+= trae la cadena completa del texto
     }
     
     }
@@ -28,9 +37,21 @@ window.cipher = {
   decode: (offset,texto) => {
     let resultado = '';
     offset= parseInt(offset);
+    if (offset <0){
+      offset= offset *-1 ;}
 
   for (let i=0; i<texto.length; i++){
     let letraAscii=texto[i].charCodeAt();
+
+    //if (letraAscii >= 160 && letraAscii <= 255 ) {
+    //  let letraConvertida=(letraAscii-160-offset)%26+160;
+    //  if (letraConvertida<160){
+    //    letraConvertida+=96;
+    //  }
+    //  resultado += String.fromCharCode(letraConvertida);
+
+
+
     if (letraAscii >= 65 && letraAscii <= 90 ) {
       let letraConvertida=(letraAscii-65-offset)%26+65;
       if (letraConvertida<65){
